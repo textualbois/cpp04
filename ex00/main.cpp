d125e6aad1469ae167353de840cb93532d0aa718
textualbois <wivna360@hotmail.com>
2024-12-17 02:12:14 +0100

    just messing

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,15 +10,21 @@ int main( void )
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	const Dog* k = static_cast<const Dog*>(j);
+	const Animal* meta2 = new Animal(*meta);
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
+	std::cout << k->getType() << " " << std::endl;
 	i->makeSound();
 	j->makeSound();
+	k->makeSound();
 	meta->makeSound();
+	meta2->makeSound();
 
 	delete meta;
 	delete j;
 	delete i;
+	delete meta2;
 
 	// Incorrect way using WrongAnimal
 	const WrongAnimal* wrongMeta = new WrongAnimal();
